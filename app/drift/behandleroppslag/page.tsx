21cf6bcf4ca5fa7e7f9907f2bd0b69819002952a
Nicolas N. <1224956+Watercolours@users.noreply.github.com>
2025-11-28 07:40:46 +0100

    legge til sjekk av rolle på behandleroppslag

--- app/drift/behandleroppslag/page.tsx
+++ app/drift/behandleroppslag/page.tsx
@@ -1,98 +1,23 @@
-'use client';
+import { Page, PageBlock } from '@navikt/ds-react/Page';
+import { Behandleroppslag } from 'components/drift/behandleroppslag/Behandleroppslag';
+import { hentRollerForBruker, Roller } from 'lib/azure/azureUserService';
+import { Alert } from '@navikt/ds-react';
 
-import { Alert, BodyShort, Button, Heading, HStack, Loader, Page, Table, TextField, VStack } from '@navikt/ds-react';
-import { useState } from 'react';
-import { Behandler } from 'lib/types';
-
-const OppslagPage = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<any>(undefined);
-  const [result, setResult] = useState<Behandler[]>([]);
-
-  const [request, setRequest] = useState({ saksnummer: '', fritekst: '' });
-
-  const søkEtterBehandler = async () => {
-    setIsLoading(true);
-    fetch('/api/drift/behandleroppslag', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ saksnummer: request.saksnummer, fritekst: request.fritekst }),
-    })
-      .then(async (res) => {
-        if (!res.ok) {
-          setError(`${res.status} ${res.statusText}: ${await res.text()}`);
-        } else {
-          setResult(await res.json());
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        setError(err);
-      })
-      .finally(() => setIsLoading(false));
-  };
+const OppslagPage = async () => {
+  const roller = await hentRollerForBruker();
 
   return (
     <Page>
-      <Page.Block width="lg">
-        <VStack gap="2" marginBlock="8">
-          <Heading size={'large'}>Behandleroppslag</Heading>
-
-          <BodyShort>
-            Søk etter behandler via dokumentinnhenting. Gjør det enklere å debugge feil knyttet til
-            behandleropplysninger.
-          </BodyShort>
-
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              søkEtterBehandler()
-            }}
-          >
-            <HStack gap="2" align="end">
-              <TextField
-                label="Saksnummer"
-                value={request.saksnummer}
-                onChange={(e) => setRequest({ ...request, saksnummer: e.target.value })}
-              />
-              <TextField
-                label="Fritekst"
-                value={request.fritekst}
-                onChange={(e) => setRequest({ ...request, fritekst: e.target.value })}
-              />
-              <Button>Søk</Button>
-            </HStack>
-          </form>
-
-          {error && <Alert variant="error">{error}</Alert>}
-        </VStack>
-        {isLoading ? (
-          <Loader />
+      <PageBlock width="lg">
+        {roller.includes(Roller.DRIFT) ? (
+          <Behandleroppslag />
         ) : (
-          <Table size="small">
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell>Behandlernavn</Table.HeaderCell>
-                <Table.HeaderCell>Kontor</Table.HeaderCell>
-                <Table.HeaderCell>Behandlerref</Table.HeaderCell>
-                <Table.HeaderCell>HPR-nr</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {result?.map((behandler) => (
-                <Table.Row key={behandler.behandlerRef}>
-                  <Table.DataCell>
-                    {behandler.fornavn} {behandler.mellomnavn || ''} {behandler.etternavn}
-                  </Table.DataCell>
-                  <Table.DataCell>{behandler.kontor}</Table.DataCell>
-                  <Table.DataCell>{behandler.behandlerRef}</Table.DataCell>
-                  <Table.DataCell>{behandler.hprId}</Table.DataCell>
-                </Table.Row>
-              ))}
-            </Table.Body>
-          </Table>
+          <Alert variant="warning">
+            Du har ikke tilgang til denne siden. AD-rollen <strong>0000-GA-AAP_DRIFT</strong> er påkrevd for å gjøre
+            oppslag.
+          </Alert>
         )}
-      </Page.Block>
+      </PageBlock>
     </Page>
   );
 };
---
+++ components/drift/behandleroppslag/Behandleroppslag.tsx
@@ -0,0 +1,98 @@
+'use client';
+
+import { Alert, BodyShort, Button, Heading, HStack, Loader, Table, TextField, VStack } from '@navikt/ds-react';
+import { useState } from 'react';
+import { Behandler } from 'lib/types';
+
+export const Behandleroppslag = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<any>(undefined);
+  const [result, setResult] = useState<Behandler[]>([]);
+
+  const [request, setRequest] = useState({ saksnummer: '', fritekst: '' });
+
+  const søkEtterBehandler = async () => {
+    if (!request.saksnummer) return setError('Saksnummer må fylles ut.');
+    if (!request.fritekst) return setError('Fritekst må fylles ut.');
+
+    setIsLoading(true);
+    fetch('/api/drift/behandleroppslag', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ saksnummer: request.saksnummer, fritekst: request.fritekst }),
+    })
+      .then(async (res) => {
+        if (!res.ok) {
+          setError(`${res.status} ${res.statusText}: ${await res.text()}`);
+        } else {
+          setResult(await res.json());
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        setError(err);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  return (
+    <>
+      <VStack gap="2" marginBlock="8">
+        <Heading size={'large'}>Behandleroppslag</Heading>
+
+        <BodyShort>
+          Søk etter behandler via dokumentinnhenting. Gjør det enklere å debugge feil knyttet til behandleropplysninger.
+        </BodyShort>
+
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            søkEtterBehandler();
+          }}
+        >
+          <HStack gap="2" align="end">
+            <TextField
+              label="Saksnummer"
+              value={request.saksnummer}
+              onChange={(e) => setRequest({ ...request, saksnummer: e.target.value })}
+            />
+            <TextField
+              label="Fritekst"
+              value={request.fritekst}
+              onChange={(e) => setRequest({ ...request, fritekst: e.target.value })}
+            />
+            <Button>Søk</Button>
+          </HStack>
+        </form>
+
+        {error && <Alert variant="error">{error}</Alert>}
+      </VStack>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Table size="small">
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Behandlernavn</Table.HeaderCell>
+              <Table.HeaderCell>Kontor</Table.HeaderCell>
+              <Table.HeaderCell>Behandlerref</Table.HeaderCell>
+              <Table.HeaderCell>HPR-nr</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {result?.map((behandler) => (
+              <Table.Row key={behandler.behandlerRef}>
+                <Table.DataCell>
+                  {behandler.fornavn} {behandler.mellomnavn || ''} {behandler.etternavn}
+                </Table.DataCell>
+                <Table.DataCell>{behandler.kontor}</Table.DataCell>
+                <Table.DataCell>{behandler.behandlerRef}</Table.DataCell>
+                <Table.DataCell>{behandler.hprId}</Table.DataCell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      )}
+    </>
+  );
+};
