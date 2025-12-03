48197cefee31e4aaf7239a9e3b073925ca1c2740
Fredrik Meyer <fredrik.meyer@nav.no>
2025-12-03 16:09:39 +0100

    Fiks typo i path

--- app/api/drift/postmottak/[referanse]/prosesser/route.ts
+++ app/api/drift/postmottak/[referanse]/prosesser/route.ts
@@ -2,13 +2,14 @@ import { NextRequest } from 'next/server';
 import { triggProsesserBehandlingIPostmottak } from '../../../../../../lib/services/driftService';
 
 interface Params {
-  behandlingsreferanse: string;
+  referanse: string;
 }
 
 export async function POST(req: NextRequest, { params }: { params: Promise<Params> }) {
-  const { behandlingsreferanse } = await params;
+  const { referanse } = await params;
+  console.log(referanse);
   try {
-    return new Response(await triggProsesserBehandlingIPostmottak(behandlingsreferanse), { status: 200 });
+    return new Response(await triggProsesserBehandlingIPostmottak(referanse), { status: 200 });
   } catch (err: any) {
     return new Response(err?.message, { status: 500 });
   }
