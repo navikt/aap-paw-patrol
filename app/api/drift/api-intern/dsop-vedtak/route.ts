a635607fd29cc563320d12957ab78ba6c1c8ba0a
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2026-04-22 19:53:11 +0200

    Bruk NextResponse.json for å hente json

--- app/api/drift/api-intern/dsop-vedtak/route.ts
+++ app/api/drift/api-intern/dsop-vedtak/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { fetchProxy } from '../../../../../lib/services/fetchProxy';
 import { getBaseUrlAndScopeForApp } from '../../../../../lib/services/driftService';
 
@@ -6,7 +6,7 @@ export async function POST(req: NextRequest) {
   const { baseUrl, scope } = await getBaseUrlAndScopeForApp("api-intern");
   const body = await req.json()
   try {
-    return new Response(await fetchProxy(`${baseUrl}/kelvin/dsop/vedtak`, scope, 'POST', body));
+    return NextResponse.json(await fetchProxy(`${baseUrl}/kelvin/dsop/vedtak`, scope, 'POST', body));
   } catch (err: any) {
     return new Response(err?.message, { status: 500 });
   }
