5b41a8a342e7a3384179bb1a1fb4e83f219b14bb
Nicolas N. <nicolas.nordhagen@nav.no>
2026-07-07 13:10:33 +0200

    AAP-2194 Ikke logge error i Paw Patrol

--- lib/services/fetchProxy.ts
+++ lib/services/fetchProxy.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import { requestAzureOboToken, validateToken } from '@navikt/oasis';
-import { getAccessTokenOrRedirectToLogin, logError } from '@navikt/aap-felles-utils';
+import { getAccessTokenOrRedirectToLogin, logWarning } from '@navikt/aap-felles-utils';
 import { headers } from 'next/headers';
 import { hentLocalToken } from 'lib/services/localTokenService';
 
@@ -14,19 +14,19 @@ const NUMBER_OF_RETRIES = 3;
 const getOnBefalfOfToken = async (audience: string, url: string): Promise<string> => {
   const token = getAccessTokenOrRedirectToLogin(await headers());
   if (!token) {
-    logError(`Token for ${url} er undefined`);
+    logWarning(`Token for ${url} er undefined`);
     throw new Error('Token for simpleTokenXProxy is undefined');
   }
 
   const validation = await validateToken(token);
   if (!validation.ok) {
-    logError(`Token for ${url} validerte ikke (errortype=${validation.errorType})`, validation.error);
+    logWarning(`Token for ${url} validerte ikke (errortype=${validation.errorType})`, validation.error);
     throw new Error('Token for simpleTokenXProxy didnt validate');
   }
 
   const onBehalfOf = await requestAzureOboToken(token, audience);
   if (!onBehalfOf.ok) {
-    logError(`Henting av oboToken for ${url} feilet`, onBehalfOf.error);
+    logWarning(`Henting av oboToken for ${url} feilet`, onBehalfOf.error);
     throw new Error('Request oboToken for simpleTokenXProxy failed');
   }
 
@@ -83,17 +83,17 @@ export const fetchWithRetry = async <ResponseBody>(
       try {
         responseJson = await response.json();
       } catch (error) {
-        logError('Klarte ikke parse JSON fra response', error);
+        logWarning('Klarte ikke parse JSON fra response', error);
         throw new Error('Klarte ikke parse JSON fra response');
       }
-      logError(`klarte ikke å hente ${url}: ${responseJson.message}`);
+      logWarning(`klarte ikke å hente ${url}: ${responseJson.message}`);
       throw new Error(`Unable to fetch ${url}: ${responseJson.message}`);
     }
     if (response.status === 404) {
       throw new Error(`Ikke funnet: ${url}`);
     }
 
-    logError(
+    logWarning(
       `Kall mot ${url} feilet med statuskode ${response.status}, prøver på nytt. Antall forsøk igjen: ${retries}`
     );
     return await fetchWithRetry(url, method, oboToken, retries - 1, requestBody, tags);
--- lib/services/localTokenService.ts
+++ lib/services/localTokenService.ts
@@ -1,17 +1,17 @@
-import { logError } from '@navikt/aap-felles-utils';
+import { logWarning } from '@navikt/aap-felles-utils';
 
 export async function hentLocalToken(scope: string) {
   try {
     const params = new URLSearchParams({
       aud: scope,
       NAVident: 'Z123456',
-      groups: 'drift-rolle'
+      groups: 'drift-rolle',
     });
 
     const url = new URL('https://fakedings.intern.dev.nav.no/fake/aad?' + params.toString());
     return fetch(url, { method: 'POST', next: { revalidate: 0 } }).then((token) => token.text());
   } catch (err) {
-    logError('hentLocalToken feilet', err);
+    logWarning('hentLocalToken feilet', err);
     return Promise.resolve('dummy-token');
   }
 }
