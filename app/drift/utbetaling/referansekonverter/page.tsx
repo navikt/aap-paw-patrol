033cd515b5186297df96596ef94da669292c5f62
Frode Lindås <frode.lindas@nav.no>
2026-07-15 15:03:59 +0200

    AAP-2354: Fjerner krav om drift rolle.

--- app/drift/utbetaling/referansekonverter/page.tsx
+++ app/drift/utbetaling/referansekonverter/page.tsx
@@ -1,23 +1,11 @@
-import { Alert } from '@navikt/ds-react';
 import { Page, PageBlock } from '@navikt/ds-react/Page';
 import { ReferanseKonverter } from 'components/drift/utbetaling/ReferanseKonverter';
-import { hentRollerForBruker, Roller } from 'lib/azure/azureUserService';
 
-const ReferanseKonverterPage = async () => {
-  const roller = await hentRollerForBruker();
-  const harTilgang = roller.includes(Roller.DRIFT);
-
+const ReferanseKonverterPage = () => {
   return (
     <Page>
       <PageBlock width="lg">
-        {harTilgang ? (
-          <ReferanseKonverter />
-        ) : (
-          <Alert variant="warning">
-            Du har ikke tilgang til denne siden. AD-rollen <strong>0000-GA-AAP_DRIFT</strong> er påkrevd for å gjøre
-            oppslag.
-          </Alert>
-        )}
+        <ReferanseKonverter />
       </PageBlock>
     </Page>
   );
