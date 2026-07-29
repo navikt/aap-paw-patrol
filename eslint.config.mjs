7074164cc97e5e676e19e91c8165d79f51eb5d6b
Fredrik Meyer <fredrik.meyer@nav.no>
2026-07-29 08:53:13 +0200

    Fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,12 +13,28 @@ const compat = new FlatCompat({
 });
 
 export default [
-  ...compat.extends('@navikt/eslint-config-aap'),
+  ...compat.extends('@navikt/eslint-config-aap').map((config) => ({
+    files: ['**/*.{js,cjs,mjs,jsx,ts,tsx}'],
+    ...config,
+  })),
   {
+    files: ['**/*.{js,cjs,mjs,jsx,ts,tsx}'],
     settings: {
       react: {
         version: '19.2.8',
       },
+    },
+    rules: {
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    rules: {
+      'no-undef': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
     },
   },
 ];
